4dc57e46d0e8ac4f4c296889daf81d1ab4b97b39
arjunkdot <78548960+arjunkdot@users.noreply.github.com>
2025-08-16 22:18:47 +0530

    Day 0 - Introduction

---
+++ day_0/euclidean_distance.py
@@ -0,0 +1,9 @@
+import math
+
+# Find Euclidan distance - One dimension
+
+def find_euclidian_distance(p, q):
+	return math.sqrt((p - q) ** 2) 
+
+
+print(find_euclidian_distance(3, 4))
---
+++ day_0/helloworld.py
@@ -0,0 +1,46 @@
+# Day 0 - 30DaysOfPython Challenge
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# Printing string
+
+print('Arjun K.')
+print('Rhythm')
+print('India')
+print('I am enjoying 30 days of python')
+
+# Checking data types
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Arjun', 'Python', 'India']))
+print(type('Arjun K.'))
+print(type('Rhythm'))
+print(type('India'))
+
+# Different data types
+
+print(6)
+print(6.6)
+print(4 - 3j)
+print('Arjun K.')
+print(True)
+print(['One', 2, False])
+print('Arjun', 'Dundu', 'Bella')
+print({4, 5, 3, 6})
+print({
+ 'first_name': 'Arjun',
+ 'last_name': 'K.',
+ 'country': 'India',
+ 'age': 1000,
+ 'is_married': True,
+ 'skills': ['JS', 'React', 'Node', 'Python']
+})
